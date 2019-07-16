f815b6e34894b006cadfe12125d7460dcb6e8eef
Grzegorz Beń <grzegorz.ben@coderush.pl>
2019-07-16 18:49:14 +0200

    orm class

--- src/orm.ts
+++ src/orm.ts
@@ -1,12 +1,12 @@
 // import { ClassInfo, FromFiles, ModuleBase } from "@spinajs/core";
-// import { OrmDriver } from "./interfaces";
+import { OrmDriver } from "./interfaces";
 // import { ModelBase } from "./model";
 
 
-// export class Orm extends ModuleBase{
+export class Orm{
 
-//     @FromFiles("/**/*.{ts,js}", "system.dirs.models")
-//     public Models: Promise<Array<ClassInfo<ModelBase>>>;
+    // @FromFiles("/**/*.{ts,js}", "system.dirs.models")
+    // public Models: Promise<Array<ClassInfo<ModelBase>>>;
 
-//     public Connections: Map<string, OrmDriver>;
-// }
+    public Connections: Map<string, OrmDriver>;
+}
